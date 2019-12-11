013ad9cd3b77cb02feb61cfc4475c68af023c3cf
milancurcic <caomaco@gmail.com>
2019-12-11 15:23:49 -0500

    load fetch values programmatically

---
+++ sustain_drag_2020/fetch.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+fetch = {
+    'irgason1': 7.15,
+    'irgason2': 11.98,
+    'pitot': 9.87,
+    'static_pressure': np.array([1.98, 3.81, 5.64, 7.47, 9.00, 11.13, 12.96, 14.79]),
+    'udm': np.array([3.34, 7.08, 9.87, 11.94, 14.90])
+}
